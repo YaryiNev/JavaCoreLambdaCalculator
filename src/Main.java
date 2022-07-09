65114cc372511df28e18402470a2ee21f4106a5f
YaryiNev <strees@bk.ru>
2022-07-09 15:16:08 +0500

    clean comment

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,11 @@ public class Main {
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        //int c = calc.devide.apply(a, b); // результат работы int b равен нулю.(делить на ноль нельзя
         try {
             int c = calc.devide.apply(a, b); // результат работы int b равен нулю.(делить на ноль нельзя
             calc.println.accept(c);
         } catch (ArithmeticException e) {
             System.out.println("Ошибка деления: на ноль делить нельзя!");
         }
-        //int c = calc.isPositive.test(b) ? calc.devide.apply(a, b) : b;
-
     }
 }
